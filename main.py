2338716054d690665801faac3cce6cc7554f0306
lmarkiewicznr2-ui <lmarkiewicznr2@gmail.com>
2025-10-24 00:02:16 +0200

    D: changed constants.py, added player.py, modified main.py to use player.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,20 +9,22 @@ def main():
 
     pygame.init()
     clock = pygame.time.Clock()
-    dt = clock.tick(60) / 1000  # Amount of seconds between each loop
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
     running = True
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while running:
         # Handle events
+        dt = clock.tick(60) / 1000  # Amount of seconds between each loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False  # stops the loop when user clicks close
 
         # Fill the screen black
         screen.fill((0, 0, 0))
+
+        player.draw(screen)  # Draw the player
 
         # Refresh the display
         pygame.display.flip()
